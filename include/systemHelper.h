b567db964f55c211f9a18dce620e4055a5ee292f
mean <fixounet@free.fr>
2026-01-03 08:13:01 +0100

    esp: handle espressif extra arg to vPortEnterCritical

--- include/systemHelper.h
+++ include/systemHelper.h
@@ -8,9 +8,16 @@ extern "C"
 {
     void deadEnd(int code);
 }
+#ifdef CRITICAL_SECTION_EXTRA_ARG
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+extern portMUX_TYPE my_spinlock;
+#define ENTER_CRITICAL() vPortEnterCritical(&my_spinlock)
+#define EXIT_CRITICAL() vPortExitCritical(&my_spinlock)
+#else
+#define ENTER_CRITICAL() vPortEnterCritical()
+#define EXIT_CRITICAL() vPortExitCritical()
+#endif
 
-#define ENTER_CRITICAL vPortEnterCritical
-#define EXIT_CRITICAL vPortExitCritical
-
-extern "C" void ENTER_CRITICAL(void);
-extern "C" void EXIT_CRITICAL(void);
+// extern "C" void ENTER_CRITICAL(void);
+// extern "C" void EXIT_CRITICAL(void);
