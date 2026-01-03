6ec59f86051488324526017ecb0223ea0323894e
mean <fixounet@free.fr>
2026-01-03 15:02:31 +0100

    esp: fixup gpio

--- mcus/riscv_esp32/src/ln_esp_gpio.cpp
+++ mcus/riscv_esp32/src/ln_esp_gpio.cpp
@@ -5,16 +5,15 @@
 extern "C"
 {
 #include "soc/gpio_periph.h"
-//#include "/register/soc/gpio_struct.h
-//#include "register/soc/gpio_struct.h"
-#include "hal/gpio_types.h" 
+#include "hal/gpio_types.h"
+#include "soc/gpio_reg.h"
 }
 /**
  */
 void lnPinMode(const lnPin pin, const lnGpioMode mode, const int speedInMhz)
 {
     gpio_config_t io_conf = {.pin_bit_mask = (1ULL << pin),
-                             .mode = GPIO_MODE_OUTPUT,
+                             .mode = GPIO_MODE_INPUT,
                              .pull_up_en = GPIO_PULLUP_DISABLE,
                              .pull_down_en = GPIO_PULLDOWN_DISABLE,
                              .intr_type = GPIO_INTR_DISABLE};
@@ -22,49 +21,31 @@ void lnPinMode(const lnPin pin, const lnGpioMode mode, const int speedInMhz)
     switch (mode)
     {
     case lnFLOATING:
-        io_conf.mode = GPIO_MODE_DISABLE;
     case lnINPUT_FLOATING:
-        xAssert(0);
         break;
     case lnINPUT_PULLUP:
-        io_conf.mode = GPIO_MODE_INPUT;
         io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
         break;
     case lnINPUT_PULLDOWN:
-        io_conf.mode = GPIO_MODE_INPUT;
         io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
         break;
     case lnOUTPUT:
+        io_conf.mode = GPIO_MODE_OUTPUT;
         break;
     case lnOUTPUT_OPEN_DRAIN:
-        xAssert(0);
+        io_conf.mode = GPIO_MODE_OUTPUT_OD;
+        io_conf.pull_up_en = GPIO_PULLUP_ENABLE; // no sure ???
         break;
     case lnALTERNATE_PP:
-        xAssert(0);
-        break;
     case lnALTERNATE_OD:
-        xAssert(0);
-        break;
     case lnPWM:
-        xAssert(0);
-        break;
     case lnADC_MODE:
-        xAssert(0);
-        break;
     case lnDAC_MODE:
-        xAssert(0);
-        break;
     case lnUART:
-        xAssert(0);
-        break;
     case lnSPI_MODE:
-        xAssert(0);
-        break;
     case lnUART_Alt:
-        xAssert(0);
-        break;
     default:
-        xAssert(0);
+        Logger("Ignoring pin configuration, assuming you have done it elsewhere");
         break;
     }
     gpio_config(&io_conf);
@@ -101,15 +82,16 @@ void lnOpenDrainClose(const lnPin pin, const bool close) // if true, the open dr
     xAssert(0);
 }
 /**
- *
+ * Assume we'll only use lower pins (0..31)
  */
+ volatile uint32_t* const GPIO_SET_REG = (volatile uint32_t*)GPIO_OUT_W1TS_REG;
+ volatile uint32_t* const GPIO_CLR_REG = (volatile uint32_t*)GPIO_OUT_W1TC_REG;
 lnFastIO::lnFastIO(lnPin p)
 {
-    lnPinMode(p, lnOUTPUT);
-    gpio_dev_t *hw = &GPIO;
-    _bit = 1 << (uint32_t)p;
-    _on = &(hw->out_w1ts.val);
-    _off = &(hw->out_w1tc.val);
+     lnPinMode(p, lnOUTPUT);
+     _on = GPIO_SET_REG;
+     _off = GPIO_CLR_REG;
+     _bit =  1<<(uint32_t )p;
 }
 
 //  EOF
