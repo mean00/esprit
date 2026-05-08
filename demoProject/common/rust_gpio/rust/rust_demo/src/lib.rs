ac68dbdccb59f3e107ae81c9ac89f29e24a3df8d
mean <fixounet@free.fr>
2026-05-08 17:18:41 +0200

    simpler example

--- demoProject/common/rust_gpio/rust/rust_demo/src/lib.rs
+++ demoProject/common/rust_gpio/rust/rust_demo/src/lib.rs
@@ -1,34 +1,32 @@
 #![no_std]
-//use rnarduino::rn_os_helper;
+
 use rust_esprit::delay_ms;
 use rust_esprit::{GpioMode::lnINPUT_PULLUP, digital_write, pin_mode};
 use rust_esprit::{lnLogger, lnLogger_init, pin};
 
 lnLogger_init!();
+
 #[cfg(any(feature = "rp2040"))]
 const PIN: pin = pin::GPIO10;
 #[cfg(not(any(feature = "rp2040")))]
 const PIN: pin = pin::PA4;
 
-struct MyStruct<T> {
-    context: T,
+struct MyStruct {
+    context: bool,
 }
-impl<T> rust_esprit::PinCallback for MyStruct<T> {
+
+impl rust_esprit::PinCallback for MyStruct {
     fn on_interrupt(&mut self, pin: rust_esprit::pin) {
         panic!("on_interrupt\n");
     }
 }
 
-/**
- *
- *
- */
 #[unsafe(no_mangle)]
 extern "C" fn user_init() {
     lnLogger!("Hello there !\n");
 
     pin_mode(PIN, lnINPUT_PULLUP);
-    let s: MyStruct<bool> = MyStruct { context: true };
+    let s: MyStruct = MyStruct { context: true };
     rust_esprit::exti_attach_interrupt_typed(PIN, rust_esprit::pin_edge::LN_EDGE_BOTH, &s);
     let mut on: bool = false;
     for _i in 0..5 {
@@ -39,5 +37,3 @@ extern "C" fn user_init() {
 
     lnLogger!("--end--\n");
 }
-
-// EOF
